6181194b96fa9fb02c3bf34b19cae815801c3afb
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-11-13 03:25:50 -0500

    Guide prints

--- mutpy/operators/security.py
+++ mutpy/operators/security.py
@@ -1,6 +1,6 @@
 import ast
 
-from mutpy.operators.base import MutationResign, MutationOperator, AbstractUnaryOperatorDeletion
+from mutpy.operators.base import MutationResign, MutationOperator, AbstractUnaryOperatorDeletion, copy_node
 
 
 class SecurityOperatorDeletion(AbstractUnaryOperatorDeletion):
@@ -16,12 +16,19 @@ class AbstractSecurityOperatorReplacement(MutationOperator):
         if self.should_mutate(node):
             return ast.Mult()
         raise MutationResign()
-    
-    def mutate_Eq_to_Lt(self, node):
-        return ast.Lt()
 
+
+class ConditionalOperatorInsertion(MutationOperator):
+    def double_negate_test(self, node):
+        not_node = ast.UnaryOp(op=ast.Not(), operand=node.test)
+        not_not_node = ast.UnaryOp(op=ast.Not(), operand=not_node)
+        node.test = not_not_node
+        return node
+
+    @copy_node
+    def mutate_If(self, node):
+        return self.double_negate_test(node)
 
 class SecurityOperatorReplacement(AbstractSecurityOperatorReplacement):
     def should_mutate(self, node):
         return not isinstance(node.parent, ast.AugAssign)
-
